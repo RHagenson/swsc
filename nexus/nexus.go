12dcbddd4bfd94990b43a3cb836a601d294c573a
Ryan Hagenson <Ryan.Hagenson@gmail.com>
2018-10-19 15:32:25 -0500

    Removed unused function

--- nexus/nexus.go
+++ nexus/nexus.go
@@ -200,20 +200,3 @@ func blockByName(s, b string) bool {
 	return strings.HasSuffix(strings.ToUpper(s), strings.ToUpper(" "+b+";"))
 }
 
-// readLines reads a whole file into memory
-// and returns a slice of its lines.
-func readLines(path string) ([]string, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	// TODO: Unnecessary allocations
-	var lines []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	return lines, scanner.Err()
-}

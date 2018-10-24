f42800e4a2c3d839f16a35be1649076b7b6d4eed
Ryan Hagenson <Ryan.Hagenson@gmail.com>
2018-10-24 16:20:26 -0500

    Comment out non-working multinomial metric

--- metrics.go
+++ metrics.go
@@ -1,58 +1,56 @@
 package main
 
 import (
-	"math"
-
 	"github.com/biogo/biogo/seq/multi"
 )
 
-func sitewiseMulti(uceAln *multi.Multi) []float64 {
-	uceCounts := sitewiseBaseCounts(uceAln)
-	uceSums := make([]float64, len(uceCounts))
-	for i := range uceSums {
-		for _, v := range uceCounts {
-			for _, z := range v {
-				uceSums[i] += float64(z)
-			}
-		}
-	}
-	uceObsFactorial := make([]float64, len(uceSums))
-	for i, v := range uceSums {
-		uceObsFactorial[i] = factorial(int(v))
-	}
-	uceFactorials := factorialMatrix(uceCounts)
+// func sitewiseMulti(uceAln *multi.Multi) []float64 {
+// 	uceCounts := sitewiseBaseCounts(uceAln)
+// 	uceSums := make([]float64, len(uceCounts))
+// 	for i := range uceSums {
+// 		for _, v := range uceCounts {
+// 			for _, z := range v {
+// 				uceSums[i] += float64(z)
+// 			}
+// 		}
+// 	}
+// 	uceObsFactorial := make([]float64, len(uceSums))
+// 	for i, v := range uceSums {
+// 		uceObsFactorial[i] = factorial(int(v))
+// 	}
+// 	uceFactorials := factorialMatrix(uceCounts)
 
-	m1 := minFloat64(uceObsFactorial...)
-	m2 := minFloat64(uceFactorials...)
-	if m1 < 0 || m2 < 0 {
-		panic("Sitewise multinomial factorials <0")
-	}
-	sitewiseLikelihood := sitewiseMultiCounts(uceCounts, uceFactorials, uceObsFactorial)
-	logLikelihoods := make([]float64, len(sitewiseLikelihood))
-	for i, v := range sitewiseLikelihood {
-		logLikelihoods[i] = math.Log(v)
-	}
-	return logLikelihoods
-}
+// 	m1 := minFloat64(uceObsFactorial...)
+// 	m2 := minFloat64(uceFactorials...)
+// 	if m1 < 0 || m2 < 0 {
+// 		panic("Sitewise multinomial factorials <0")
+// 	}
+// 	sitewiseLikelihood := sitewiseMultiCounts(uceCounts, uceFactorials, uceObsFactorial)
+// 	logLikelihoods := make([]float64, len(sitewiseLikelihood))
+// 	for i, v := range sitewiseLikelihood {
+// 		logLikelihoods[i] = math.Log(v)
+// 	}
+// 	return logLikelihoods
+// }
 
-// Was not in the sample code
-// Implemented similar to sitewiseBaseCount
-func sitewiseMultiCounts(uceAln *multi.Multi) map[byte][]float64 {
-	counts := map[byte][]float64{
-		'A': make([]float64, uceAln.Len()),
-		'T': make([]float64, uceAln.Len()),
-		'C': make([]float64, uceAln.Len()),
-		'G': make([]float64, uceAln.Len()),
-	}
-	for i := 0; i < uceAln.Len(); i++ {
-		site, _ := uceAln.Subseq(i, i+1)
-		bCounts := sitewiseMulti(site)
-		for k, v := range bCounts {
-			counts[k][i] += v
-		}
-	}
-	return counts
-}
+// // Was not in the sample code
+// // Implemented similar to sitewiseBaseCount
+// func sitewiseMultiCounts(uceAln *multi.Multi) map[byte][]float64 {
+// 	counts := map[byte][]float64{
+// 		'A': make([]float64, uceAln.Len()),
+// 		'T': make([]float64, uceAln.Len()),
+// 		'C': make([]float64, uceAln.Len()),
+// 		'G': make([]float64, uceAln.Len()),
+// 	}
+// 	for i := 0; i < uceAln.Len(); i++ {
+// 		site, _ := uceAln.Subseq(i, i+1)
+// 		bCounts := sitewiseMulti(site)
+// 		for k, v := range bCounts {
+// 			counts[k][i] += v
+// 		}
+// 	}
+// 	return counts
+// }
 
 func sitewiseEntropy(aln *multi.Multi) []float64 {
 	entropies := make([]float64, aln.Len())

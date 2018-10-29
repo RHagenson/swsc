1303f063d8abbaba2eed8dcde830607797a4f7e4
Ryan Hagenson <Ryan.Hagenson@gmail.com>
2018-10-29 08:43:22 -0500

    Move sitewiseGCc to be with other metric

--- metrics.go
+++ metrics.go
@@ -78,3 +78,18 @@ func sitewiseBaseCounts(uceAln *multi.Multi) map[byte][]int {
 	}
 	return counts
 }
+
+func sitewiseGc(uceAln *multi.Multi) []float64 {
+	gc := make([]float64, uceAln.Len())
+	for i := range gc {
+		site := uceAln.Column(i, false)
+		// TODO: Will not compute properly if using lowercase letters
+		for _, nuc := range site {
+			if byte(nuc) == 'G' || byte(nuc) == 'C' {
+				gc[i]++
+			}
+		}
+		gc[i] = gc[i] / float64(uceAln.Rows())
+	}
+	return gc
+}

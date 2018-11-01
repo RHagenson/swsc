949f391f94596ab195057785a9acb933f67ea2b9
Ryan Hagenson <Ryan.Hagenson@gmail.com>
2018-11-01 10:02:10 -0500

    Use Letters() and add FillFrom === Read outputs tests

--- nexus/nexus_test.go
+++ nexus/nexus_test.go
@@ -2,6 +2,7 @@ package nexus_test
 
 import (
 	"os"
+	"reflect"
 	"strings"
 	"testing"
 
@@ -16,6 +17,33 @@ func TestNexus(t *testing.T) {
 		t.Fatalf("Could not open example input: %s\n", datafile)
 	}
 	nex.FillFrom(in)
+
+	t.Run("FillFrom matches Read", func(t *testing.T) {
+		rin, _ := os.Open(datafile)
+		rnex := nexus.Read(rin)
+
+		if nex.NTax() != rnex.NTax() {
+			t.Errorf("NTax: FillFrom got %v, Read got %v", nex.NTax(), rnex.NTax())
+		}
+		if nex.NChar() != rnex.NChar() {
+			t.Errorf("NChar: FillFrom got %v, Read got %v", nex.NChar(), rnex.NChar())
+		}
+		if nex.DataType() != rnex.DataType() {
+			t.Errorf("DataType: FillFrom got %v, Read got %v", nex.DataType(), rnex.DataType())
+		}
+		if nex.Gap() != rnex.Gap() {
+			t.Errorf("Gap: FillFrom got %v, Read got %v", nex.Gap(), rnex.Gap())
+		}
+		if nex.Missing() != rnex.Missing() {
+			t.Errorf("Missing: FillFrom got %v, Read got %v", nex.Missing(), rnex.Missing())
+		}
+		if !reflect.DeepEqual(nex.Charsets(), rnex.Charsets()) {
+			t.Errorf("Charsets: FillFrom got %v, Read got %v", nex.Charsets(), rnex.Charsets())
+		}
+		if !reflect.DeepEqual(nex.Alignment(), rnex.Alignment()) {
+			t.Errorf("Alignment: FillFrom got %v, Read got %v", nex.Alignment(), rnex.Alignment())
+		}
+	})
 
 	t.Run("Data", func(t *testing.T) {
 		t.Run("NTax", func(t *testing.T) {
@@ -91,15 +119,29 @@ func TestNexus(t *testing.T) {
 				}
 			})
 			t.Run("Seqs only contain ATGC+missing+gap characters", func(t *testing.T) {
-				allowed := "ATGC" + string(nex.Missing()) + string(nex.Gap())
 				for i := uint(0); i < aln.NSeq(); i++ {
 					seq := aln.Seq(i)
 					sum := 0
-					for _, r := range allowed {
+					for _, r := range nex.Letters() {
 						sum += strings.Count(seq, string(r))
 					}
 					if sum != len(seq) {
 						t.Errorf("Sequence contains invalid characters.")
+					}
+				}
+			})
+			t.Run("String", func(t *testing.T) {
+				str := aln.String()
+				seqs := strings.Split(strings.TrimSpace(str), "\n")
+				for i := 0; i < len(seqs); i++ {
+					if len(seqs[i]) != len(seqs[i/len(seqs)]) {
+						t.Errorf("Lengths of sequences %d and %d do not match at %d and %d, respectively",
+							i, i/len(seqs), len(seqs[i]), len(seqs[i/len(seqs)]))
+					}
+				}
+				for i := aln.Len(); i < len(str); i += aln.Len() + 1 {
+					if str[i] != '\n' {
+						t.Errorf("Newline expected, got %q (range %q)", str[i], str[i-5:i+5])
 					}
 				}
 			})

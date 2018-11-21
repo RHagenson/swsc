9e2a93af194d1e107befe2c74e4ba8a6a4d7fcde
Ryan Hagenson <Ryan.Hagenson@gmail.com>
2018-11-21 09:51:54 -0600

    Errorf test for expected os.Exit(2)

---
+++ ui/errorf_test.go
@@ -0,0 +1,23 @@
+package ui_test
+
+import (
+	"os"
+	"os/exec"
+	"testing"
+
+	"bitbucket.org/rhagenson/swsc/ui"
+)
+
+func TestErrorf(t *testing.T) {
+	if os.Getenv("BE_CRASHER") == "2" {
+		ui.Errorf("%s", "testing exit code 2")
+		return
+	}
+	cmd := exec.Command(os.Args[0], "-test.run=TestErrorf")
+	cmd.Env = append(os.Environ(), "BE_CRASHER=2")
+	err := cmd.Run()
+	if e, ok := err.(*exec.ExitError); ok && !e.Success() {
+		return
+	}
+	t.Fatalf("process ran with err %v, want exit status 2", err)
+}

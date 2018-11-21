33f3beb2bc88bdba3ab1bee511777005fb376b46
Ryan Hagenson <Ryan.Hagenson@gmail.com>
2018-11-21 09:52:11 -0600

    Removed unecessary function call

--- ui/ui.go
+++ ui/ui.go
@@ -7,12 +7,10 @@ import (
 
 // Header informs the user what work is being performed
 func Header(f string) {
-	fmt.Println()
-	fmt.Printf("Analysing %s\n", path.Base(f))
+	fmt.Printf("\nAnalysing %s\n", path.Base(f))
 }
 
 // Footer informs the user of what work was just performed
 func Footer(f string) {
-	fmt.Println()
-	fmt.Printf("Wrote partitions to %s\n", f)
+	fmt.Printf("\nWrote partitions to %s\n", f)
 }

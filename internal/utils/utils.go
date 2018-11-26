ef23be27bd0c8281677b08ee33c49e0588e8f66d
Ryan Hagenson <Ryan.Hagenson@gmail.com>
2018-11-26 15:51:41 -0600

    Remove unused code

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -3,7 +3,6 @@ package utils
 import (
 	"fmt"
 	"math"
-	"math/big"
 
 	"github.com/pkg/errors"
 )
@@ -18,49 +17,6 @@ func ValidateMinWin(length, minWin int) error {
 		return errors.New(msg)
 	}
 	return nil
-}
-
-func minFloat64(vs ...float64) float64 {
-	min := math.MaxFloat64
-	for _, v := range vs {
-		if v < min {
-			min = v
-		}
-	}
-	return min
-}
-
-func factorial(v int) (float64, error) {
-	fact := big.NewFloat(1)
-	for i := 1; i <= v; i++ {
-		fact.Mul(fact, big.NewFloat(float64(i)))
-	}
-	val, acc := fact.Float64()
-	if acc == big.Exact {
-		return val, nil
-	}
-	return val, errors.Errorf("factorial of %d was %s the true value", v, acc)
-}
-
-func factorialMatrix(vs map[byte][]int) ([]float64, error) {
-	length := 0
-	for _, v := range vs {
-		length = len(v)
-	}
-	product := make([]float64, length) // vs['A'][i] * vs['T'][i] * vs['G'][i] * vs['C'][i]
-	for i := range product {
-		product[i] = 1.0
-	}
-	for i := range product {
-		for nuc := range vs {
-			val, err := factorial(vs[nuc][i])
-			product[i] *= val
-			if err != nil {
-				return nil, fmt.Errorf("%v", err)
-			}
-		}
-	}
-	return product, nil
 }
 
 // MinInCountsMap returns the minimum value in the map

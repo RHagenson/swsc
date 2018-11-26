2b2370f24ea8ef9f6b32e76b8134d957b7de4904
Ryan Hagenson <Ryan.Hagenson@gmail.com>
2018-11-26 09:32:04 -0600

    Make ui more testable by fmt'ing string only

--- ui/ui.go
+++ ui/ui.go
@@ -6,11 +6,11 @@ import (
 )
 
 // Header informs the user what work is being performed
-func Header(f string) {
-	fmt.Printf("\nAnalysing %s\n", path.Base(f))
+func Header(f string) string {
+	return fmt.Sprintf("\nAnalysing %s\n", path.Base(f))
 }
 
 // Footer informs the user of what work was just performed
-func Footer(f string) {
-	fmt.Printf("\nWrote partitions to %s\n", f)
+func Footer(f string) string {
+	return fmt.Sprintf("\nWrote partitions to %s\n", f)
 }

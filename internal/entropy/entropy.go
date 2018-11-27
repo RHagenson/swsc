d79a91f726288b7add833037386839357f267927
Ryan Hagenson <Ryan.Hagenson@gmail.com>
2018-11-27 10:28:46 -0600

    Preallocate array

--- internal/entropy/entropy.go
+++ internal/entropy/entropy.go
@@ -16,14 +16,16 @@ func AlignmentEntropy(aln nexus.Alignment, chars []byte) float64 {
 
 // entropyCalc computes Shannon's entropy after removing elements equal to zero as Ln(0) == -Inf
 func entropyCalc(bpFreqs map[byte]float64) float64 {
-	freqs := make([]float64, 0)
+	freqs := make([]float64, len(bpFreqs))
+	i := 0
 	for _, val := range bpFreqs {
 		// Ln(0) == -Inf, Shannon's entropy uses Ln()
 		if val != 0 {
-			freqs = append(freqs, float64(val))
+			freqs[i] = float64(val)
 		} else {
-			freqs = append(freqs, math.SmallestNonzeroFloat64)
+			freqs[i] = math.SmallestNonzeroFloat64
 		}
+		i++
 	}
 	return stat.Entropy(freqs)
 }
